9aed01f57a7227ec939386f17f932c564d0744b2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-17 18:16:06 +0530

    Method 2 Created and varabile added

--- Demo2/src/com/lti/model/User.java
+++ Demo2/src/com/lti/model/User.java
@@ -4,9 +4,14 @@ public class User {
 	public void method(){
 		System.out.println("xc");
 	}
+	
+	public void method2(){
+		System.out.println("method 2");
+	}
 	public static void main(String[] args) {
 		
 		int x =1;
+		int  y=101;
 		System.out.println("ashlesh khajbage");
 	}
 }
