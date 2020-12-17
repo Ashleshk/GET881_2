e87206a3c87e6a7eb230592560bf743b97694926
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-12-17 18:11:53 +0530

    new repo Commit

---
+++ Demo2/src/com/lti/model/User.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class User {
+	public void method(){
+		System.out.println("xc");
+	}
+	public static void main(String[] args) {
+		
+		int x =1;
+		System.out.println("ashlesh khajbage");
+	}
+}
